58dc0a4291ad06bbfa93d0b2215f53a259a4d08d
msamgan <mohdsamgankhan@gmail.com>
2021-02-07 00:41:02 +0400

    updated git ignore

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const Index = require('../controllers/api/index')
-
-module.exports = function(app){
-    /**
-     * static URLS to be on top..
-     */
-    app.get('/api', Index.index)
-    app.post('/api', Index.indexPost)
-}
--- routes/web.js
+++
@@ -1,9 +0,0 @@
-const Index = require('../controllers/index')
-
-module.exports = function(app){
-    /**
-     * static URLS to be on top..
-     */
-    app.get('/', Index.index)
-    app.post('/', Index.indexPost)
-}
